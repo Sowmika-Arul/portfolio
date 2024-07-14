5c01d88ef8d2a02a026d015630a00f349407e26f
Sowmika-Arul <sowmikaarulkumar@gmail.com>
2024-07-14 20:30:55 +0530

    changes

--- new/src/App.js
+++ new/src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import Navbar from './components/navbar';
 import './App.css';
+import Navbar from './components/Navbar.js';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <section id="about">
-        <h1>About Me</h1>
-        <p>This is the about section.</p>
-      </section>
-      <section id="projects">
-        <h1>Projects</h1>
-        <p>This is the projects section.</p>
-      </section>
-      <section id="contact">
-        <h1>Contact</h1>
-        <p>This is the contact section.</p>
-      </section>
+    <div>
+    <Navbar/>
     </div>
   );
 }
