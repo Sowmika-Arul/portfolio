5647b8f3808f77c15e31e180697a416db052840c
Sowmika-Arul <sowmikaarulkumar@gmail.com>
2024-07-14 23:19:58 +0530

    changes in css

--- new/src/App.js
+++ new/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
-import Navbar from './components/Navbar.js';
+// Adjust the path accordingly
 
 function App() {
   return (
-    <div>
-    <Navbar/>
+    <div className="App">
+      <Navbar />
+      <div className="main-content">
+        <div className="content">
+         <pre>Hello, I'm 
+         Sowmika</pre>
+        </div>
+        <div className="photo">
+        <div className="profile-image-wrapper">
+        <img src="your-photo-url.jpg" alt="Your Name" className="profile-image" />
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
--- new/src/components/navbar.js
+++ new/src/components/navbar.js
@@ -5,10 +5,11 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <a href="#">My Portfolio</a>
+        <a href="#new">SOWMIKA</a>
       </div>
       <ul className="navbar-links">
-        <li><a href="#about">About</a></li>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#resumee">Resume</a></li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
